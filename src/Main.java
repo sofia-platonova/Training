7e8ed2316e24fc240ec8dc2c760f4feb3186f7df
sofia-platonova <141740025+sofia-platonova@users.noreply.github.com>
2023-10-04 11:47:43 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,7 @@ public class Main {
         // Press Alt+Enter with your caret at the highlighted text to see how
         // IntelliJ IDEA suggests fixing it.
         System.out.println("Hello and welcome!");
-        System.out.println("Another one commit");
+        System.out.println("Another one committtt");
         System.out.println("It is a commit");
         // Press Shift+F10 or click the green arrow button in the gutter to run the code.
         for (int i = 1; i <= 5; i++) {
